b17f732848c9a5fb49538bb22e831b9f387cf869
Walid White <walidwhite@live.co.uk>
2025-01-14 15:27:39 +0000

    Set up database, T-shirt model, admin panel, and .gitignore

--- LaVérité/settings.py
+++ LaVérité/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-wgwhitecoding-laverite-fc0ydddnty8.ws.codeinstitute-ide.net'
+]
