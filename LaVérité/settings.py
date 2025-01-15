728d39c796da4aea5d28a79554565a32ed114c17
Walid White <walidwhite@live.co.uk>
2025-01-15 21:26:38 +0000

    heroku deployment

--- LaVérité/settings.py
+++ LaVérité/settings.py
@@ -37,6 +37,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -51,19 +52,19 @@ ROOT_URLCONF = 'LaVérité.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # Ensure 'templates' directory is included
+        'DIRS': [BASE_DIR / 'templates'],  # This points to your current structure
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request',  # Required by Allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'tshirt.context_processors.cart_item_count',  # Custom context processor
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'LaVérité.wsgi.application'
 
@@ -71,6 +72,7 @@ WSGI_APPLICATION = 'LaVérité.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
+
 
 # Password Validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -90,6 +92,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
